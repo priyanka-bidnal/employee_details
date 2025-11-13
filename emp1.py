452b822f4b252ba14e57a1d00b591bb3a3bf3814
priyanka-bidnal <bidnalpriyanka@gmail.com>
2025-11-13 16:57:10 +0530

    Create emp1.py

---
+++ emp1.py
@@ -0,0 +1,8 @@
+employee_name="Raj"
+employee_id=12345
+employee_salary=30000
+employee_address="Hubli"
+print("Employee name:",employee_name)
+print("Employee id=",employee_id)
+print("Employee salary=",employee_salary)
+print("Employee address=",employee_address)
